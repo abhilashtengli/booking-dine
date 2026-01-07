0b1f468c7ed2bf357132bf93e572ac7531adb01b
Abhilash <abhilasht0007.t@gmail.com>
2026-01-07 12:33:32 +0530

    Update : home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,20 +1,56 @@
 import React from "react";
 import {
+  ActivityIndicator,
+  FlatList,
   Image,
   ImageBackground,
+  ListRenderItem,
   Platform,
   ScrollView,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { BlurView } from "expo-blur";
+import { restaurants } from "@/store/resturant";
 const logo = require("@/assets/images/dinetimelogo.png");
 const banner = require("@/assets/images/homeBanner.png");
 
+type Restaurant = {
+  name: string;
+  seats: number;
+  image: string; // URL
+  address: string;
+  opening: string;
+  closing: string;
+};
+
 export default function Home() {
+  const renderItem: ListRenderItem<Restaurant> = ({ item }) => (
+    <TouchableOpacity className="bg-[#5f5f5f] rounded-lg shadow-md flex p-4 mx-4 max-h-64 max-w-xs">
+      <Image
+        resizeMode="cover"
+        source={{ uri: item.image }}
+        className="h-28  mb-1 rounded-lg  border-red-500 "
+      />
+      <Text className="mr-4 font-semibold mb-1 text-lg text-white ">
+        {item.name}
+      </Text>
+      <Text className="mr-4  mb-1 text-sm text-white ">{item.address}</Text>
+      <Text className="mr-4  mb-1 text-sm text-white ">
+        {item.opening} - Close: {item.closing}
+      </Text>
+    </TouchableOpacity>
+  );
   return (
-    <SafeAreaView style={{ backgroundColor: "#2b2b2b" }}>
+    <SafeAreaView
+      style={[
+        { backgroundColor: "#2b2b2b" },
+        Platform.OS === "android" && { paddingBottom: 60 },
+        Platform.OS === "ios" && { paddingBottom: 30 },
+      ]}
+    >
       <View className="flex items-center">
         <View className="bg-[#5f5f5f] w-11/12 rounded-lg shadow-lg justify-between items-center my-3">
           <View className="flex flex-row   border-red-400 ">
@@ -25,10 +61,10 @@ export default function Home() {
           </View>
         </View>
       </View>
-      <ScrollView>
+      <ScrollView stickyHeaderIndices={[0]}>
         <ImageBackground
           resizeMode="cover"
-          className="my-4 w-full h-52 items-center justify-center"
+          className="mb-4 w-full h-52 items-center justify-center bg-[#2b2b2b]"
           source={banner}
         >
           <BlurView
@@ -41,6 +77,36 @@ export default function Home() {
             </Text>
           </BlurView>
         </ImageBackground>
+        <View className="p-4 bg-[#2b2b2b] flex-row items-center">
+          <Text className="text-3xl text-white mr-2 font-semibold">
+            Special Discount %
+          </Text>
+        </View>
+        {restaurants.length > 0 ? (
+          <FlatList
+            data={restaurants}
+            renderItem={renderItem}
+            horizontal
+            contentContainerStyle={{ padding: 16 }}
+          />
+        ) : (
+          <ActivityIndicator animating color={"fb9b33"} />
+        )}
+        <View className="p-4 bg-[#2b2b2b] flex-row items-center">
+          <Text className="text-3xl text-[#fb9b33] mr-2 font-semibold">
+            Our Resturants
+          </Text>
+        </View>
+        {restaurants.length > 0 ? (
+          <FlatList
+            data={restaurants}
+            renderItem={renderItem}
+            horizontal
+            contentContainerStyle={{ padding: 16 }}
+          />
+        ) : (
+          <ActivityIndicator animating color={"fb9b33"} />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
