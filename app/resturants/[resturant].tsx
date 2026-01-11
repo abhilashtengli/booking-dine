a8ef314dae066e2eeba423cd63774ebadc711ece
Abhilash <abhilasht0007.t@gmail.com>
2026-01-11 16:15:32 +0530

    Update : carousel UI done

--- app/resturants/[resturant].tsx
+++ app/resturants/[resturant].tsx
@@ -1,16 +1,132 @@
-import { Text, Platform, ScrollView, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import {
+  Text,
+  Platform,
+  ScrollView,
+  View,
+  FlatList,
+  Dimensions,
+  Image,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/config/firebaseConfig";
 import { CarouselItem, Restaurant, SlotItem } from "@/utils/types";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Resturant() {
+  const windowWidth = Dimensions.get("window").width;
   const [resturantData, setResturantData] = useState<Restaurant | null>(null);
   const [carouselData, setCarouselData] = useState<CarouselItem[] | null>(null);
   const [slotsData, setSlotsData] = useState<SlotItem[] | null>(null);
   const { resturant } = useLocalSearchParams();
+  const flatlistRef = useRef<FlatList<string>>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextImage = () => {
+    const carouselLength = carouselData?.[0]?.images.length;
+
+    if (currentIndex < carouselLength! - 1) {
+      const nextIndex = currentIndex + 1;
+      setCurrentIndex(nextIndex);
+      flatlistRef.current?.scrollToIndex({ index: nextIndex, animated: true });
+    }
+
+    if (currentIndex === carouselLength! - 1) {
+      setCurrentIndex(0);
+      flatlistRef.current?.scrollToIndex({ index: 0, animated: true });
+    }
+  };
+  const handlePrevImage = () => {
+    const carouselLength = carouselData?.[0]?.images.length;
+
+    if (currentIndex > 0) {
+      const prevIndex = currentIndex - 1;
+      setCurrentIndex(prevIndex);
+      flatlistRef.current?.scrollToIndex({ index: prevIndex, animated: true });
+    }
+
+    if (currentIndex === 0) {
+      const prevIndex = carouselLength! - 1;
+      setCurrentIndex(prevIndex);
+      flatlistRef.current?.scrollToIndex({ index: prevIndex, animated: true });
+    }
+  };
+  const caraouselItem = ({ item }: { item: string }) => {
+    return (
+      <View style={{ width: windowWidth - 2 }} className="h-64 relative">
+        <View
+          style={{
+            position: "absolute",
+            top: "50%",
+            backgroundColor: "rgba(0,0,0,0.1)",
+            borderRadius: 50,
+            padding: 5,
+            zIndex: 10,
+            right: "6%",
+          }}
+        >
+          <Ionicons
+            onPress={handleNextImage}
+            name="arrow-forward"
+            size={18}
+            color="white"
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: "50%",
+            backgroundColor: "rgba(0,0,0,0.1)",
+            borderRadius: 50,
+            padding: 5,
+            zIndex: 10,
+            left: "2%",
+          }}
+        >
+          <Ionicons
+            onPress={handlePrevImage}
+            name="arrow-back"
+            size={18}
+            color="white"
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "row",
+            left: "50%",
+            transform: [{ translateX: -50 }],
+            zIndex: 10,
+            bottom: 10,
+          }}
+        >
+          {carouselData?.[0]?.images.map((_, index) => (
+            <View
+              key={index}
+              className={`bg-white opacity-15 ${index === currentIndex && "h-2 w-2 opacity-80"} h-2 w-2 p-1 mx-1 rounded-full`}
+            />
+          ))}
+        </View>
+        <View>
+          <Image
+            source={{ uri: item }}
+            style={{
+              opacity: 0.5,
+              backgroundColor: "black",
+              marginRight: 20,
+              marginLeft: 5,
+              borderRadius: 15,
+            }}
+            className="h-64"
+          />
+        </View>
+      </View>
+    );
+  };
 
   const getResturantData = async () => {
     try {
@@ -29,7 +145,7 @@ export default function Resturant() {
         const resturantData = doc.data() as Restaurant;
         setResturantData(resturantData);
 
-        console.log("R data : ", resturantData);
+        // console.log("R data : ", resturantData);
 
         //Carousel
 
@@ -69,7 +185,7 @@ export default function Resturant() {
         );
 
         setSlotsData(slots);
-        console.log("Slots : ", slots);
+        // console.log("Slots : ", slots);
       }
     } catch (e) {
       console.log(e);
@@ -94,6 +210,18 @@ export default function Resturant() {
           </Text>
         </View>
         <View className="border-b-2 border-[#f49b33]" />
+
+        <View className="h-64 max-w-[98%] mx-2 rounded-[25px] mt-8">
+          <FlatList
+            ref={flatlistRef}
+            data={carouselData?.[0]?.images}
+            renderItem={caraouselItem}
+            horizontal
+            scrollEnabled={false}
+            style={{ borderRadius: 15 }}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
